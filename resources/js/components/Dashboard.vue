b17fcbf32279b487b10fa94ed9ed045494605d93
Your Name <thanh@eocenterprise.com>
2018-11-06 21:46:06 +0700

    set up

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,60 @@
+<template>
+	<div id="app">
+	  	<v-app id="inspire">
+		    <v-navigation-drawer
+		      fixed
+		      :clipped="$vuetify.breakpoint.mdAndUp"
+		      app
+		      v-model="drawer"
+		      :mini-variant.sync="mini"
+		    >
+		    <side-bar></side-bar>  
+		    </v-navigation-drawer>
+		    <v-toolbar
+		      color="red"
+		      dark
+		      app
+		      :clipped-left="$vuetify.breakpoint.mdAndUp"
+		      fixed
+		    >
+			    <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+			        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+			        <span class="hidden-sm-and-down">Cook Cook</span>
+			    </v-toolbar-title>
+	    	</v-toolbar>
+	    	<v-content>
+              	<v-container>
+                  	<router-view></router-view>
+              	</v-container>
+            </v-content>
+	    	
+  		</v-app>
+	</div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import SideBar from './partials/Sidebar.vue'
+ 
+Vue.use(Vuetify)
+export default {
+
+  	name: 'Dashboard',
+
+	data () {
+	    return {
+	    	drawer: null,
+	    }
+	},
+
+	components: {
+      	'side-bar':SideBar,
+    },
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ resources/js/components/partials/Sidebar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <v-list dense class="pt-0">
+      <v-list-tile
+        v-for="item in items"
+        :key="item.title"
+        :to="item.link"
+        ripple="ripple"
+        :exact="item.exact" 
+        class="black--text"
+        active-class="red--text"
+      >
+        <v-list-tile-action>
+            <v-icon color="red">{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+            <v-list-tile-title color="red">{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+</div>
+</template>
+
+<script>
+export default {
+
+  name: 'Sidebar',
+
+  data () {
+    return {
+        items: [
+          { title: 'Home', icon: 'dashboard', link: '/'},
+          { title: 'About', icon: 'people', link: 'example' }
+        ]
+    }
+  },
+}
+</script>
+
+<style lang="css" scoped>
+</style>
