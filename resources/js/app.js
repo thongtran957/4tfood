bfc563f7804765976c0db87419fcd7470a471212
Thong Tran <38139264+thongtran957@users.noreply.github.com>
2018-11-27 13:42:01 +0700

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -32,7 +32,7 @@ import Register from './components/modules/register/Index.vue'
 
 var access_token = localStorage.getItem('access_token')
 
-axios.defaults.baseURL = 'http://chefguidecenter.herokuapp.com/';
+// axios.defaults.baseURL = 'http://chefguidecenter.herokuapp.com/';
 axios.defaults.headers.common['Authorization'] =  access_token;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
