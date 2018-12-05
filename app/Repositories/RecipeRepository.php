ad80f7eb21367841489a8fa688ed271758e2b7fc
Your Name <thanh@eocenterprise.com>
2018-12-05 10:42:56 +0700

    edit

--- app/Repositories/RecipeRepository.php
+++ app/Repositories/RecipeRepository.php
@@ -118,7 +118,7 @@ class RecipeRepository extends BaseRepository
     public function edit($name_img, $link_img, $input){
         $category_id = Category::select('id')->where('name',$input['cname'])->first()->toArray();
         $category_id = $category_id['id'];
-        
+        dd($input['active']);
         if($input['active'] != null){
             $active = 1;
         }else{
